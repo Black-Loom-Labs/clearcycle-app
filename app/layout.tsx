22522b548d115b4cde3797db5192c118087722cc
gauravhira <gauravhira24@gmail.com>
2026-08-26 06:11:10 +0530

    feat: favicon svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "ClearCycle",
   description: "Insurance claims pipeline management for Indian hospitals",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
